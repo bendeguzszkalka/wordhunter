197c02127e89556328c94b518e833640d364ec61
Bendeguz Szkalka <bendeguz.szkalka@gmail.com>
2023-07-19 01:05:17 +0200

    Random selection of word from wordlist

--- WordHunter/Program.cs
+++ WordHunter/Program.cs
@@ -2,6 +2,21 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string wordsFilePath = @"enter path of words.wh";
+		try
+		{
+            string[] words = File.ReadAllLines(wordsFilePath);
+            Console.WriteLine(randomWord(words));
+        }
+		catch (IOException exception)
+		{
+            Console.WriteLine($"Error reading file: {exception.Message}");
+        }
+    }
+
+    static string randomWord(string[] words)
+    {
+        Random random = new Random();
+        return words[random.Next(0, words.Length)];
     }
 }
